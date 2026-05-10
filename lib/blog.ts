28ec7bd9540bc979d6f8a67137f8634c92bef680
jeremyecker <jeremy@resteasypest.com>
2026-05-09 23:59:35 -0400

    Content: How to Get Rid of Cockroaches in NYC Apartments
    * Add blog post: How to Get Rid of Cockroaches in NYC Apartments
    
    * Register cockroaches NYC apartments post in blog.ts

--- lib/blog.ts
+++ lib/blog.ts
@@ -20,6 +20,7 @@ import p19 from "@/data/blog/what-attracts-cockroaches-nyc-apartments.json";
 import p20 from "@/data/blog/why-bug-bombs-dont-work-nyc-apartments.json";
 import p21 from "@/data/blog/how-to-check-for-bed-bugs-nyc-apartment.json";
 import p22 from "@/data/blog/nyc-ant-season-spring-apartment-control.json";
+import p23 from "@/data/blog/how-to-get-rid-of-cockroaches-nyc-apartments.json";
 
 export interface BlogPost {
   slug: string;
@@ -37,7 +38,7 @@ export interface BlogPost {
 const RAW_POSTS: any[] = [
   p01, p02, p03, p04, p05, p06, p07, p08, p09, p10,
   p11, p12, p13, p14, p15, p16, p17, p18, p19, p20,
-  p21, p22,
+  p21, p22, p23,
 ];
 
 export const ALL_POSTS: BlogPost[] = RAW_POSTS.map((p) => ({
